0689d8be7884c51b655e8132eb02f729c410e41f
Mayra Diaz <mayra.diaz@utec.edu.pe>
2023-04-10 10:30:55 -0600

    Initial Commit

---
+++ Space/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Space
+//
+//  Created by Mayra Díaz Tramontana on 10/04/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
